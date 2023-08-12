e428ffc9d1236b4866b9ce73acedeaaf1a7bb631
flan02 <chanivetdan@hotmail.com>
2023-08-11 22:23:46 -0300

    Ya podemos registratarnos en mongodb e iniciar sesion pegando a los usuarios que esten en mongodb.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { connectDB } from '@/mongodb/mongodb'
+import User from '@/models/user'
+import bcrypt from 'bcryptjs'
 
 //? NextAuth tiene Databases y guarda directamente en la base de dato
 //? Recordar qe una cosa es el TOKEN & otra cosa es la SESSION
@@ -15,9 +18,16 @@ const handler = NextAuth({
                 email: { label: "Email", type: "email", placeholder: "some@hotmail.com" },
                 password: { label: "Password", type: "password", placeholder: "********" }
             },
-            authorize(credentials, req) {
-                const user = { id: "1", fullname: 'flan02', email: 'chanivetdan@hotmail.com' }
-                return user
+            async authorize(credentials, req) {
+                //const user = { id: "1", fullname: 'flan02', email: 'chanivetdan@hotmail.com' }
+                await connectDB()
+                //console.log(credentials);
+                const userFound = await User.findOne({ email: credentials?.email }).select("+password") //? El + es p/ añada el campo password
+                if (!userFound) throw new Error("Invalid credentials") //! Nunca especificar que datos son invalidos.
+                const passwordMatch = await bcrypt.compare(credentials!.password, userFound.password)
+                if (!passwordMatch) throw new Error("Invalid credentials") //! Nunca especificar que datos son invalidos.
+                console.log(userFound);
+                return userFound
             }
         })
     ],
@@ -26,13 +36,14 @@ const handler = NextAuth({
             /*   console.log({account,token,user,profile,}); */
             // * Aqui podemos modificar el token p/q guarde los datos qe ya tiene.
             // token.hello = "hello world"
-            if (user) token.user = user
+            if (user) token.user = user //? Lo qe recibe de mongo lo guarda en el token
             //console.log(token);
             return token
         },
         session({ session, token }) {
             //console.log(session);
-            session.user = token.user as any //* crear interfaz, esto aparece en el frontend
+            //TODO crear interfaz nunca ANY, esto aparece en el frontend
+            session.user = token.user as any //? La sesion tendra en el navegador data del token
             return session
         },
     }
